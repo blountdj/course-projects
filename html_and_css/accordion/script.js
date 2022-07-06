d4bce2be0fa3fed306bab871667494631e93b45e
blountdj <blountdj@gmail.com>
2022-07-06 16:33:38 +0200

    Update script.js

--- html_and_css/accordion/script.js
+++ html_and_css/accordion/script.js
@@ -6,4 +6,13 @@ items.forEach(function (item) {
 
 function toggleClass() {
   this.classList.toggle("open");
+
+  let panel = this.querySelector(".hidden-box");
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+    panel.style.paddingTop = "0px";
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    panel.style.paddingTop = "40px";
+  }
 }
